5f86939153b8aa9e71d1daffb5a9c57048df3b4b
Ezejosue <35981484+Ezejosue@users.noreply.github.com>
2020-11-08 18:05:52 -0600

    Conexion

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/CONEXION.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/CONEXION.java
@@ -4,14 +4,39 @@
  * and open the template in the editor.
  */
 package com.mycompany.mavenproject1;
+
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+
 /**
  *
  * @author aezeq11
  */
 public class CONEXION {
-    
-    
+
+    private String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private String url = "jdbc:sqlserver://localhost:1433;databaseName=NORTHWIND";
+    private String user = "sa";
+    private String pass = "123456";
+
+    CONEXION() {
+    }
+
+    public Connection conectar() {
+        try {
+            Class.forName(driver);
+            return (DriverManager.getConnection(url, user, pass));
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+    public void CerrarConexion(Connection con) {
+        try {
+            con.close();
+        } catch (Exception e) {
+        }
+    }
 }
